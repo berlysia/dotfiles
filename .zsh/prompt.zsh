8a8e923201df0ca2d213b9618c0c98c822af96e7
berlysia <berlysia@gmail.com>
2021-11-20 19:03:35 +0900

    update prompt

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -6,35 +6,49 @@ typeset -A emoji
 emoji[ok]=$'\U2705'
 emoji[error]=$'\U274C'
 emoji[git]=$'\U1F500'
-emoji[git_changed]=$'\U1F37A'
-emoji[git_untracked]=$'\U1F363'
+emoji[git_staged]=$'\U1F4DD'
+emoji[git_changed]=$'\U1F4AC'
+emoji[git_untracked]=$'\U2757'
 emoji[git_clean]=$'\U2728'
 emoji[right_arrow]=$'\U2794'
 
-function _vcs_git_indicator () {
+local vcs_git_indicator
+function __vcs_git_indicator () {
   typeset -A git_info
   local git_indicator git_status
   git_status=("${(f)$(git status --porcelain --branch 2> /dev/null)}")
   (( $? == 0 )) && {
     git_info[branch]="${${git_status[1]}#\#\# }"
     shift git_status
-    git_info[changed]=${#git_status:#\?\?*}
-    git_info[untracked]=$(( $#git_status - ${git_info[changed]} ))
-    git_info[clean]=$(( $#git_status == 0 ))
+    git_info[all]=$#git_status
+    git_info[tracked]=${#git_status:#\?\?*} # untrackじゃないもの
+    git_info[untracked]=$(( ${git_info[all]} - ${git_info[tracked]}))
+    git_info[staged]=$(( ${#git_status:# ?*} - ${git_info[untracked]} )) # 1文字目が空白じゃなくてuntrackではないもの（untrackが/^??/にマッチする前提
+    git_info[changed]=${#git_status:#? *} # 2文字目が空白じゃないもの
+    git_info[clean]=$(( ${git_info[all]} == 0 ))
 
     git_indicator=("${emoji[git]}  %{%F{blue}%B%}${git_info[branch]}%{%b%f%}")
     (( ${git_info[clean]}     )) && git_indicator+=("${emoji[git_clean]}")
-    (( ${git_info[changed]}   )) && git_indicator+=("${emoji[git_changed]}  %{%F{yellow}%}${git_info[changed]} changed%{%f%}")
-    (( ${git_info[untracked]} )) && git_indicator+=("${emoji[git_untracked]}  %{%F{red}%}${git_info[untracked]} untracked%{%f%}")
+    (( ${git_info[staged]}    )) && git_indicator+=("${emoji[git_staged]} %{%F{green}%}${git_info[staged]} staged%{%f%}")
+    (( ${git_info[changed]}   )) && git_indicator+=("${emoji[git_changed]} %{%F{yellow}%}${git_info[changed]} unstaged%{%f%}")
+    (( ${git_info[untracked]} )) && git_indicator+=("${emoji[git_untracked]} %{%F{red}%}${git_info[untracked]} / ${git_info[changed]} untracked%{%f%}")
   }
-  _vcs_git_indicator="${git_indicator}"
+
+  # 使うときに改行を処理したいが、すぐできなかったのでここで埋めている
+  if [ -n "$git_indicator" ]; then
+    vcs_git_indicator="${git_indicator}
+"
+  else
+    vcs_git_indicator=""
+  fi
 }
 
-add-zsh-hook precmd _vcs_git_indicator
+add-zsh-hook precmd __vcs_git_indicator
 
 function {
   local dir='%F{green}%B%~%{%b%f%}'
-  local rc="%(?,${emoji[ok]} ,${emoji[error]}  %F{red}%?%f)"
+  local rc="%(?,${emoji[ok]} ,${emoji[error]} %F{red}%?%f)"
+  local br=$'\n'
 
   local user
   if [ $UID -eq 0 ]; then
@@ -45,9 +59,24 @@ function {
 
   local host='%F{blue}%B%m%b%f'
   [ "$SSH_CLIENT" ] && local via="${${=SSH_CLIENT}[1]} %{%B%}${emoji[right_arrow]}%{%b%} "
-  local git='$_vcs_git_indicator'
-  local mark=$'\n> '
-  local br=$'\n'
+  local git='$vcs_git_indicator'
+  local mark=$'> '
+  local datetime=$'%D{%Y-%m-%d %H:%M:%S %Z(%z)}'
 
-  PROMPT="$dir $user($via$host) $rc $git $mark"
+  # 最初は表示時の時刻が出る
+  PROMPT="$dir $user($via$host)$rc %F{blue}$datetime%f $br$git$mark"
+  # コマンド実行すると実行時の時刻が残る
+  BASE_PROMPT="$dir $user($via$host)$rc %F{magenta}$datetime%f $br$git$mark"
 }
+
+function __rewrite_on_accept() {
+  if [[ ${#PROMPT} -ne ${#BASE_PROMPT} ]]; then
+    local saved=$PROMPT
+    PROMPT=$BASE_PROMPT
+    zle reset-prompt
+    PROMPT=$saved
+  fi
+  zle accept-line
+}
+zle -N __rewrite_on_accept
+bindkey '^M' __rewrite_on_accept
