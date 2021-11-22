7172a7dfc30d3aada7699b3706f886631f9b74a9
berlysia <berlysia@gmail.com>
2021-11-22 11:22:06 +0900

    introduce ~/.local/bin directory

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -1,5 +1,10 @@
 path=(
+  # 環境ごとの実行ファイルやsymlinkの置き場所
+  $HOME/.local/bin
+
+  # dotfilesで管理する実行ファイルの置き場所
   $HOME/.bin
+
   $HOME/.deno/bin
   $HOME/workspace/depot_tools
   $path
--- etc/setup/06-bat.sh
+++ etc/setup/06-bat.sh
@@ -9,7 +9,8 @@ if type brew > /dev/null 2>&1; then
 elif type apt > /dev/null 2>&1; then
   echo "install bat via apt"
   sudo apt install bat
-  ln -s /usr/bin/batcat ~/.bin/bat
+  mkdir -p ~/.local/bin
+  ln -s /usr/bin/batcat ~/.local/bin/bat
 else
   echo "bat installation skipped... see https://github.com/sharkdp/bat"
 fi
