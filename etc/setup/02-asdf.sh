21230c1893ecb7f6c10f2d540fa9886c47195325
berlysia <berlysia@gmail.com>
2022-01-22 20:21:39 +0900

    初期設定時によくある問題への注意を追加

--- etc/setup/02-asdf.sh
+++ etc/setup/02-asdf.sh
@@ -3,6 +3,10 @@ set -eu
 
 if ! type asdf; then
 
+if [ -e ~/.asdf ]; then
+  echo "初期設定時の注意: シェルの再起動はしたか？"
+fi
+
 # https://asdf-vm.com/
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 cd ~/.asdf
