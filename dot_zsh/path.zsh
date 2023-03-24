610189199a66714d52bab19f224fa01f39fedb75
berlysia <berlysia@gmail.com>
2023-03-25 01:09:51 +0900

    init

---
+++ dot_zsh/path.zsh
@@ -0,0 +1,12 @@
+path=(
+  # 環境ごとの実行ファイルやsymlinkの置き場所
+  $HOME/.local/bin
+
+  # dotfilesで管理する実行ファイルの置き場所
+  $HOME/.bin
+
+  $HOME/.deno/bin
+  $HOME/google-cloud-sdk/bin
+  $HOME/workspace/depot_tools
+  $path
+)
