0f1881ec9760fe0cf32aeabbb00e6d093556a17c
berlysia <berlysia@gmail.com>
2025-06-25 18:19:18 +0900

    bye shebang

--- dot_shell_common/env.sh
+++ dot_shell_common/env.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
 # Common environment variables for all shells
 
 # Default editor
 if type code &>/dev/null; then
-  export EDITOR="code --wait"
+  EDITOR="code --wait"
 elif type emacs &>/dev/null; then
-  export EDITOR=emacs
+  EDITOR=emacs
 fi
+export EDITOR
 
 # FZF configuration
 export FZF_DEFAULT_OPTS="--height=50% --layout=reverse --info=inline --border --padding=1"
