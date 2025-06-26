5db3d3d21f90380a8ebcc0fca7354d6efec8853e
berlysia <berlysia@gmail.com>
2025-06-27 01:46:01 +0900

    SSH config for dawrin

--- dot_shell_common/darwin.sh
+++ dot_shell_common/darwin.sh
@@ -14,6 +14,30 @@ if [ -S "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" ]
   export SSH_AUTH_SOCK="$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
 fi
 
+# 1Password SSH LaunchAgent setup (run once)
+if [[ -d "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password" ]] && [[ ! -f ~/Library/LaunchAgents/com.1password.SSH_AUTH_SOCK.plist ]]; then
+  mkdir -p ~/Library/LaunchAgents
+  cat << 'EOF' > ~/Library/LaunchAgents/com.1password.SSH_AUTH_SOCK.plist
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>Label</key>
+  <string>com.1password.SSH_AUTH_SOCK</string>
+  <key>ProgramArguments</key>
+  <array>
+    <string>/bin/sh</string>
+    <string>-c</string>
+    <string>/bin/ln -sf $HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock $SSH_AUTH_SOCK</string>
+  </array>
+  <key>RunAtLoad</key>
+  <true/>
+</dict>
+</plist>
+EOF
+  launchctl load -w ~/Library/LaunchAgents/com.1password.SSH_AUTH_SOCK.plist
+fi
+
 # macOS specific aliases
 alias showfiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder'
 alias hidefiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder'
