4adc3b01dfd4ace69974a607ae76ced9a8927ed3
berlysia <berlysia@gmail.com>
2025-06-27 01:32:12 +0900

    add SSH_AUTH_SOCK for darwin

--- dot_shell_common/darwin.sh
+++ dot_shell_common/darwin.sh
@@ -8,6 +8,12 @@ elif [ -f /usr/local/bin/brew ]; then
   eval "$(/usr/local/bin/brew shellenv)"
 fi
 
+# 1Password SSH agent
+# https://developer.1password.com/docs/ssh/get-started/
+if [ -S "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" ]; then
+  export SSH_AUTH_SOCK="$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+fi
+
 # macOS specific aliases
 alias showfiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder'
 alias hidefiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder'
